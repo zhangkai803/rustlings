9792f009850e8c4e4fe5b173a3e7d85d3cbbec15
zk <kai.zhang@lizhiweike.com>
2023-06-15 14:37:03 +0800

    resolve: some exercises

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -25,6 +23,16 @@ struct Team {
     goals_conceded: u8,
 }
 
+impl Team {
+    fn new(name: String) -> Team {
+        Team{
+            name: name,
+            goals_scored: 0,
+            goals_conceded: 0,
+        }
+    }
+}
+
 fn build_scores_table(results: String) -> HashMap<String, Team> {
     // The name of the team is the key and its associated struct is the value.
     let mut scores: HashMap<String, Team> = HashMap::new();
@@ -32,14 +40,24 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
     for r in results.lines() {
         let v: Vec<&str> = r.split(',').collect();
         let team_1_name = v[0].to_string();
-        let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
+        let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_score: u8 = v[3].parse().unwrap();
         // TODO: Populate the scores table with details extracted from the
         // current line. Keep in mind that goals scored by team_1
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        scores.entry(team_1_name.clone()).or_insert(Team::new(team_1_name.clone()));
+        scores.entry(team_2_name.clone()).or_insert(Team::new(team_2_name.clone()));
+        scores
+            .entry(team_1_name.clone())
+            .and_modify(|team| team.goals_scored += team_1_score)
+            .and_modify(|team| team.goals_conceded += team_2_score);
+        scores
+            .entry(team_2_name.clone())
+            .and_modify(|team| team.goals_scored += team_2_score)
+            .and_modify(|team| team.goals_conceded += team_1_score);
     }
     scores
 }
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples (num: i32) -> i32 {
+    let mut unit = 2;
+    if num > 40 {
+        unit = 1;
+    }
+    num * unit
+}
 
 // Don't modify this function!
 #[test]
